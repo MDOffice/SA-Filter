4f18a58c6696946075da0e6aa0b31abca1fa7014
Kelatev <kelatev@gmail.com>
2019-02-08 16:59:50 +0200

    refactoring

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,44 +1,39 @@
-import path from 'path';
-
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import rename from 'gulp-rename';
-
-import uglify from 'gulp-uglify';
 import concat from 'gulp-concat';
-
-import csso from 'gulp-csso';
 import csscomb from 'gulp-csscomb';
+import csso from 'gulp-csso';
+import gulp from 'gulp';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
 
 //  PATHS
 const paths = {
-    'js': './src/js/*.js',
-    'css': 'src/css/*.css'
+    js: './src/js/*.js',
+    css: 'src/css/*.css'
 };
 
 //  TASKS
-gulp.task('scripts', function () {
-    return gulp.src(paths.js)
+gulp.task('scripts', () =>
+    gulp.src(paths.js)
         .on('error', console.log)
         .pipe(uglify())
         .pipe(rename('sa-filter-bootstrap.js'))
         .pipe(gulp.dest('./dist/js'))
         .pipe(concat('sa-filter-bootstrap.min.js'))
-        .pipe(gulp.dest('./dist/js'));
-});
+        .pipe(gulp.dest('./dist/js'))
+);
 
-gulp.task('styles', function () {
-    return gulp.src(paths.css)
+
+gulp.task('styles', () =>
+    gulp.src(paths.css)
         .pipe(csscomb())
         .pipe(rename('sa-filter-bootstrap.css'))
         .pipe(gulp.dest('./dist/css'))
         .pipe(csso())
         .pipe(rename({
-            suffix: ".min"
+            suffix: '.min'
         }))
-        .pipe(gulp.dest('./dist/css'));
-});
+        .pipe(gulp.dest('./dist/css'))
+);
 
 const build = gulp.parallel('scripts', 'styles');
 gulp.task('build', build);
-export default build;
