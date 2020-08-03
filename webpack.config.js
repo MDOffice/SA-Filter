763e3edd14d20a313c4435271334b391922b55d3
kelatev <kelatev@gmail.com>
2020-08-03 16:15:33 +0300

    build:

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
